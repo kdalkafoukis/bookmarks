111bdc992f580f6452b165f89f7b7a32f4cd76a1
Konstantinos Dalkafoukis <kdalkafoukis@yahoo.gr>
2019-05-13 23:10:45 +0100

    workaround v0.1

--- transformbookmarks.py
+++ transformbookmarks.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python3.7
 
-import requests
+# import requests
+import urllib3
 import html2text
 import re
+# import time
 
 from getbookmarks import getBookmarks
 # from getbookmarks import printBookmarks
 
+http = urllib3.PoolManager()
+
 def filteredText(url): #take a website and tranform it to text
-    r = requests.get(url)   #make the request
+    # r = requests.get(url,verify= False)   #make the request
+    # r = requests.get(url)   #make the reques
 
-    text_maker = html2text.HTML2Text()
-    text_maker.ignore_links = True
-    text_maker.ignore_images = True
+    try:
+        r = http.request('GET', url, retries=urllib3.Retry(redirect=2, raise_on_redirect=False))
 
-    text = text_maker.handle(r.text)    #html to text
-    splitted_text = text.split()
+        text_maker = html2text.HTML2Text()
+        text_maker.ignore_links = True
+        text_maker.ignore_images = True
 
-    final_text = []
-    for i in splitted_text:             #clear the text
-        temp = re.sub("[^A-Za-z]", "", i)
-        if (temp != ''):
-            final_text.append(temp)
+        print
+        text = text_maker.handle(str(r.data.decode()))    #html to text
+        splitted_text = text.split()
 
-    return " ".join(final_text)         #make the array text again
+        final_text = []
+        for i in splitted_text:             #clear the text
+            temp = re.sub("[^A-Za-z]", "", i)
+            if (temp != ''):
+                final_text.append(temp)
+
+        return " ".join(final_text)         #make the array text again
+    except ( urllib3.exceptions.MaxRetryError) as e:
+        pass
+    # text_maker = html2text.HTML2Text()
+    # text_maker.ignore_links = True
+    # text_maker.ignore_images = True
+
+    # text = text_maker.handle(r.text)    #html to text
+    # splitted_text = text.split()
+
+    # final_text = []
+    # for i in splitted_text:             #clear the text
+    #     temp = re.sub("[^A-Za-z]", "", i)
+    #     if (temp != ''):
+    #         final_text.append(temp)
+
+    # return " ".join(final_text)         #make the array text again
 
 bookmarks = getBookmarks()
 
-text = filteredText(bookmarks[0])
-print(text)
+# text = filteredText(bookmarks[0])
+# print(text)
 
-# for bookmark in bookmarks:
-#     text = filteredText(bookmark)
-#     print(text)
+for bookmark in bookmarks:
+    
+    text = filteredText(bookmark)
+    print(text)
+
+    # time.sleep( 0.250 )
